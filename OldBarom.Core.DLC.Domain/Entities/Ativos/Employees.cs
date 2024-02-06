a64f019c39c46bcdb7eb3b3e49db5a0939e3f3ae
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-02-05 21:22:30 -0300

    👔Update Employees

--- OldBarom.Core.DLC.Domain/Entities/Ativos/Employees.cs
+++ OldBarom.Core.DLC.Domain/Entities/Ativos/Employees.cs
@@ -79,8 +79,59 @@ namespace OldBarom.Core.DLC.Domain.Entities.Ativos
 
         public Guid TerminationReasonID { get; private set; }
         public virtual TerminationReasons TerminationReason { get; private set; }
-        public Guid ODLDivision { get; private set; }
+        public Guid ODLDivisionID { get; private set; }
         public virtual ODLDivisions ODLDivision { get; private set; }
 
+        public Employees(string name)
+        {
+            ValidateDomain(name);   
+        }
+        public void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Name too short");
+            DomainExceptionValidation.When(name.Length > 100, "Name too long");
+            Name = name;
+        }
+
+        public Employees(string name, string register, string workday, Guid contractTypeID, Guid shiftID, Guid costCenterID, Guid positionID, Guid jobProfileID, Guid skillID, Guid businessID, Guid primaryOrganizationID, Guid allocationID, Guid typeID, Guid contractorID, Guid cityID, Guid genderID, Guid clockID, Guid supervisorDirectID, Guid supervisorWorkdayID, DateTime admissionDate, DateTime? demissionDate, DateTime? birthDate, DateTime? vacationStartDate, DateTime? effectiveDate, DateTime? leaveDate, DateTime? returnFromLeaveDate, DateTime? vacationEndDate, DateTime? gracePeriodReturnDate, string notes, Guid workStationID, Guid lineID, Guid disabilityTypeID, Guid restrictionTypeID, Guid terminationReasonID, Guid oDLDivisionID)
+        {
+            ValidateDomain(name);
+            Name = name;
+            Register = register;
+            Workday = workday;
+            ContractTypeID = contractTypeID;
+            ShiftID = shiftID;
+            CostCenterID = costCenterID;
+            PositionID = positionID;
+            JobProfileID = jobProfileID;
+            SkillID = skillID;
+            BusinessID = businessID;
+            PrimaryOrganizationID = primaryOrganizationID;
+            AllocationID = allocationID;
+            TypeID = typeID;
+            ContractorID = contractorID;
+            CityID = cityID;
+            GenderID = genderID;
+            ClockID = clockID;
+            SupervisorDirectID = supervisorDirectID;
+            SupervisorWorkdayID = supervisorWorkdayID;
+            AdmissionDate = admissionDate;
+            DemissionDate = demissionDate;
+            BirthDate = birthDate;
+            VacationStartDate = vacationStartDate;
+            EffectiveDate = effectiveDate;
+            LeaveDate = leaveDate;
+            ReturnFromLeaveDate = returnFromLeaveDate;
+            VacationEndDate = vacationEndDate;
+            GracePeriodReturnDate = gracePeriodReturnDate;
+            Notes = notes;
+            WorkStationID = workStationID;
+            LineID = lineID;
+            DisabilityTypeID = disabilityTypeID;
+            RestrictionTypeID = restrictionTypeID;
+            TerminationReasonID = terminationReasonID;
+            ODLDivisionID = oDLDivisionID;
+        }
     }
 }
