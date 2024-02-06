63315658791df5cec6aa9ba78f86664bcf35aaaf
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-02-05 21:00:39 -0300

    👔 Add Supervisors

---
+++ OldBarom.Core.DLC.Domain/Entities/Ativos/Supervisors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldBarom.Core.DLC.Domain.Entities.Ativos
+{
+    internal class Supervisors
+    {
+    }
+}
